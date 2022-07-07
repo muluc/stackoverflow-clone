542c431a4b2ffbc3ac310f4aba0a32f2e26feb04
Florian Feichtinger <florian@feichtinger.co>
2022-07-07 17:58:11 +0200

    changed siteUrl in config

--- client/site.config.js
+++ client/site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'TalentOverflow',
   description: 'Where Recruiters Learn & Share',
-  siteUrl: 'https://talentoverflow.vercel.app',
+  siteUrl: 'https://talentoverflow.co',
   googleAnalytic: 'G-8K89MCQEK5',
   author: {
     email: 'florian@feichtinger.co',
